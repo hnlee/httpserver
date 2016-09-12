752873cbd8c76b9621d5ee806ec01d32ba246662
Hana Lee <hanalee07@gmail.com>
2016-09-12 13:58:51 -0500

    Add ability to parse URI from request line

--- src/httpserver/request.clj
+++ src/httpserver/request.clj
@@ -1,6 +1,7 @@
 (ns httpserver.request)
 
 (defn parse [request-msg]
-  (let [regex (re-find #"^([A-Z]+).*" 
+  (let [regex (re-find #"^([A-Z]+) (.+) (HTTP.+)$" 
                        (request-msg :request-line))]
-    (hash-map :method (regex 1))))
+    (hash-map :method (regex 1)
+              :uri (regex 2))))
--- test/httpserver/request_test.clj
+++ test/httpserver/request_test.clj
@@ -3,22 +3,28 @@
             [httpserver.request :refer :all]))
 
 (deftest test-parse
-  (testing "Return hashmap with method from GET request"
+  (testing "Return hashmap from GET request"
     (let [request-msg (parse (hash-map :request-line 
                                        "GET / HTTP/1.1"))]
       (is (= "GET" (request-msg :method))))) 
-  (testing "Return hashmap with method from PUT request"
+  (testing "Return hashmap from PUT request"
     (let [request-msg (parse (hash-map :request-line
                                        "PUT / HTTP/1.1"
                                        :body
                                        "My=Data"))]
       (is (= "PUT" (request-msg :method))))) 
-  (testing "Return hashmap with method from POST request"
+  (testing "Return hashmap from POST request"
     (let [request-msg (parse (hash-map :request-line
                                        "POST / HTTP/1.1"
                                        :body
                                        "My=Data"))]
       (is (= "POST" (request-msg :method)))))
+  (testing "Return hashmap from HEAD request"
+    (let [request-msg 
+          (parse (hash-map :request-line
+                           "HEAD /foobar HTTP/1.1"))]
+      (is (= "HEAD" (request-msg :method)))
+      (is (= "/foobar" (request-msg :uri)))))
 )
 
   
