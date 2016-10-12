fa05794ffff0b7e6909b06036e8b98a98761c69b
Hana Lee <hanalee07@gmail.com>
2016-10-12 11:15:57 -0500

    Cover gaps in unit testing
    Specifically to cover the logic in read-headers and read-body.

--- httpserver/test/httpserver/socket_test.clj
+++ httpserver/test/httpserver/socket_test.clj
@@ -27,53 +27,105 @@
     (testing "Client can connect to server"
       (is (.isConnected client-socket)))))
 
-(def single-line-request (str "GET / HTTP/1.1"
+(def request-line-only-request (str "GET / HTTP/1.1"
+                                    crlf
+                                    crlf
+                                    crlf))
+
+(def single-header-request (str "GET / HTTP/1.1"
+                                crlf
+                                "Host: localhost"
+                                crlf
+                                crlf))
+
+(def multi-headers-request (str "GET / HTTP/1.1"
+                                crlf
+                                "Host: localhost"
+                                crlf
+                                "Accept: text/plain"
+                                crlf
+                                crlf))
+
+(def zero-length-request (str "PUT /form HTTP/1.1"
                               crlf
+                              "Content-Length: 0"
                               crlf
                               crlf))
 
-(def multi-line-request (str "PUT /form HTTP/1.1"
-                             crlf
-                             "Content-Length: 7"
-                             crlf
-                             crlf
-                             "My=Data"))
+(def headers-and-body-request (str "PUT /form HTTP/1.1"
+                               crlf
+                               "Content-Length: 7"
+                               crlf
+                               "Content-Type: text/plain"
+                               crlf
+                               crlf
+                               "My=Data"))
 
 (deftest test-body?
+  (testing "Request with no headers"
+    (is ((complement body?) request-line-only-request)))
   (testing "Request with no Content-Length header"
-    (is ((complement body?) single-line-request)))
-  (testing "Request with Content-Length header"
-    (is (body? multi-line-request))))
+    (is ((complement body?) single-header-request)))
+  (testing "Request with Content-Length header and body"
+    (is (body? headers-and-body-request))))
 
 (deftest test-read-request-line
   (testing "Get request line from request with no headers"
-    (with-open [input (StringReader. single-line-request)
+    (with-open [input (StringReader.
+                        request-line-only-request)
                 stream (io/reader input)]
       (is (= "GET / HTTP/1.1\r\n"
              (read-request-line stream)))))
   (testing "Get request line from request with headers"
-    (with-open [input (StringReader. multi-line-request)
+    (with-open [input (StringReader. multi-headers-request)
                 stream (io/reader input)]
-      (is (= "PUT /form HTTP/1.1\r\n"
+      (is (= "GET / HTTP/1.1\r\n"
              (read-request-line stream))))))
 
 (deftest test-read-headers
   (testing "Request with no headers"
-    (with-open [input (StringReader. single-line-request)
+    (with-open [input (StringReader.
+                        request-line-only-request)
                 stream (io/reader input)]
       (read-request-line stream)
-      (is (= crlf
+      (is (= crlf 
              (read-headers stream)))))
-  (testing "Request with headers"
-    (with-open [input (StringReader. multi-line-request)
+  (testing "Request with one headers"
+    (with-open [input (StringReader. single-header-request)
                 stream (io/reader input)]
       (read-request-line stream)
-      (is (= (str "Content-Length: 7" crlf)
-             (read-headers stream))))))
+      (is (= (str "Host: localhost" crlf)
+             (read-headers stream)))))
+   (testing "Request with multiple headers"
+    (with-open [input (StringReader. multi-headers-request)
+                stream (io/reader input)]
+      (read-request-line stream)
+      (is (= (str "Host: localhost"
+                  crlf
+                  "Accept: text/plain"
+                  crlf)
+             (read-headers stream)))))
+   (testing "Request with headers and body"
+     (with-open [input (StringReader.
+                         headers-and-body-request)
+                 stream (io/reader input)]
+       (read-request-line stream)
+       (is (= (str "Content-Length: 7"
+                   crlf
+                   "Content-Type: text/plain"
+                   crlf)
+              (read-headers stream))))))
 
 (deftest test-read-body
-  (testing "Read message body from request"
-    (with-open [input (StringReader. multi-line-request)
+  (testing "Read request with zero-length body"
+    (with-open [input (StringReader.
+                        zero-length-request)
+                stream (io/reader input)]
+      (is (= "" 
+             (read-body (read-headers stream) stream)))))
+  (testing "Read request with headers and body"
+    (with-open [input (StringReader.
+                        headers-and-body-request)
                 stream (io/reader input)]
       (is (= "My=Data"
              (read-body (read-headers stream) stream))))))
@@ -84,14 +136,19 @@
               connection (listen server-socket)
               stream (io/writer client-socket)]
     (testing "Server can get input"
-      (.write stream single-line-request)
+      (.write stream request-line-only-request)
       (.flush stream)
-      (is (= single-line-request
+      (is (= request-line-only-request 
              (receive connection))))
-    (testing "Server can get multiline input"
-      (.write stream multi-line-request)
+    (testing "Server can get request with headers"
+      (.write stream multi-headers-request)
       (.flush stream)
-      (is (= multi-line-request
+      (is (= multi-headers-request 
+             (receive connection))))
+    (testing "Server can get request with headers and body"
+      (.write stream headers-and-body-request)
+      (.flush stream)
+      (is (= headers-and-body-request 
              (receive connection))))))
 
 (deftest test-give
